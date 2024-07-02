d32b89af734d278be8a71237efb8b0f465a658cc
Florian Schaefer <168949185+RyTiXz@users.noreply.github.com>
2024-07-02 10:12:49 +0200

    Initial pseudo code to master the project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Pseudo Code
+// DONE Step 1: Setup the Project structure
+// Step 2: Write the logic to get the computer choice
+// Step 3: Write the logic to get the human choice
+// Step 4: Declare the players score variables
+// Step 5: Write the logic to play a single round
+// Step 6: Write the logic to play the entire game
