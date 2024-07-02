da97e2e0e9552c3b4d2adb73bc1e5bb703e8b3f4
Florian Schaefer <168949185+RyTiXz@users.noreply.github.com>
2024-07-02 10:16:56 +0200

    Note pseudo code for Step 2

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Pseudo Code
 // DONE Step 1: Setup the Project structure
 // Step 2: Write the logic to get the computer choice
+// function getcomputerChoice should random choose between rock paper or scissors
+// to get this, function should roll a number between 1 to 3, each one stands for one of those mentiond above
+// safe result in a variable 
+// console.log result and test the code
 // Step 3: Write the logic to get the human choice
 // Step 4: Declare the players score variables
 // Step 5: Write the logic to play a single round
